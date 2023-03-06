e3325a6c5ad254330fbbe0fa13cd9e0ef8fd0cfb
pvm <petrenkovm@hotmail.com>
2023-03-06 20:19:15 +0200

    add solution

---
+++ HappyTicket.py
@@ -0,0 +1,18 @@
+
+n = ""
+sum1 = sum2 = 0
+
+while len(n) != 6 :
+	n = input("Entered a ticket number:")
+
+for i in range(3):
+	sum1 += int(n[i])
+	sum2 += int(n[-(i+1)])
+
+if sum1==sum2:
+	print('this ticket is lucky!')
+else:
+	print("this ticket isn't lucky!")
+
+print('sum1 = ', sum1)
+print('sum2 = ', sum2)
